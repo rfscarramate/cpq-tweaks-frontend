1a1ab5623dddf2c9f161f8b1429d90d94be8bc20
root <root@localhost.localdomain>
2025-10-16 22:28:54 -0300

    Remoção de resíduos do gradle wraper local

--- gradlew.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIRNAME=%~dp0
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-set DEFAULT_JVM_OPTS=
-
-java %DEFAULT_JVM_OPTS% -cp "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
